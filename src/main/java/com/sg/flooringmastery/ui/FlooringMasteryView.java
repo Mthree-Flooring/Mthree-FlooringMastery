19f909240d9307573281ff393a054061bd4f8e18
Christian De Leon <deleon.christian21@yahoo.com>
2021-11-26 13:25:52 -0500

    Added the structure on FM View.

--- src/main/java/com/sg/flooringmastery/ui/FlooringMasteryView.java
+++ src/main/java/com/sg/flooringmastery/ui/FlooringMasteryView.java
@@ -20,108 +20,58 @@ public class FlooringMasteryView {
     public FlooringMasteryView(UserIO io) {
     this.io = io;
 }
-    public void displayDisplayStudentBanner () {
-    io.print("=== Display Student ===");
     
-}
     
-    public void displayErrorMessage(String errorMsg) {
-    io.print("=== ERROR ===");
-    io.print(errorMsg);
-}
+    public  int printMenuAndGetSelection() {
+        io.print("  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        io.print("   * <<Flooring Program>>");
+        io.print("   * 1. Display Orders");
+        io.print("   * 2. Add an Order");
+        io.print("   * 3. Edit an Order");
+        io.print("   * 4. Remove an Order");
+        io.print("   * 5. Export All Data");
+        io.print("   * 6. Quit");
+        io.print("   *");
+        io.print("   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        return io.readInt("Please select from the choices above", 1, 6);
 
-public String getStudentIdChoice() {
-    return io.readString("Please enter the Student ID.");
-}
-
-public void displayStudent(Order student) {
-    if (student != null) {
-        io.print(student.getStudentId());
-        io.print(student.getFirstName() + " " + student.getLastName());
-        io.print(student.getCohort());
-        io.print("");
-    } else {
-        io.print("No such student.");
-    }
-    io.readString("Please hit enter to continue.");
-}
-
-public void displayRemoveStudentBanner () {
-    io.print("=== Remove Student ===");
-}
-
-public void displayRemoveResult(Order studentRecord) {
-    if(studentRecord != null){
-      io.print("Student successfully removed.");
-    }else{
-      io.print("No such student.");
-    }
-    io.readString("Please hit enter to continue.");
-}
-    
-    public Order getNewStudentInfo() {
-    String studentId = io.readString("Please enter Student ID");
-    String firstName = io.readString("Please enter First Name");
-    String lastName = io.readString("Please enter Last Name");
-    String cohort = io.readString("Please enter Cohort");
-    Order currentStudent = new Order(studentId);
-    currentStudent.setFirstName(firstName);
-    currentStudent.setLastName(lastName);
-    currentStudent.setCohort(cohort);
-    return currentStudent;
-}
-    
-  public void displayExitBanner() {
-    io.print("Good Bye!!!");
-}
-
-public void displayUnknownCommandBanner() {
-    io.print("Unknown Command!!!");
-}
-    public void displayCreateStudentBanner() {
-    io.print("=== Create Student ===");
-}
-    
-    public void displayCreateSuccessBanner() {
-    io.readString(
-            "Student successfully created.  Please hit enter to continue");
-}
-    
-    public int printMenuAndGetSelection() {
-        io.print("Main Menu");
-        io.print("1. List Students");
-        io.print("2. Create New Student");
-        io.print("3. View a Student");
-        io.print("4. Remove a Student");
-        io.print("5. Exit");
-
-        return io.readInt("Please select from the above choices.", 1, 5);
     }
     
-   
-    
-    public void displayStudentList(List<Order> studentList) {
-    for (Order currentStudent : studentList) {
-        String studentInfo = String.format("#%s : %s %s",
-              currentStudent.getStudentId(),
-              currentStudent.getFirstName(),
-              currentStudent.getLastName());
-        io.print(studentInfo);
-    }
-    io.readString("Please hit enter to continue.");
-}
-
-    public void displayDisplayAllBanner() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void displayCurrentMoneyValue(BigDecimal CurrentMoney){
-        io.print("Current Balance: "+ CurrentMoney);
-    }
-
-    public void displayRemoveSuccessBanner() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
+        public String displayOrderList(List<Order> orderList) {
+        String format =  "%16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s";
+        String headings = String.format(format, 
+                "Customer name",
+                "State",
+                "Tax rate",
+                "Product",
+                "Area",
+                "Cost per square foot ($)",
+                "Labor cost per square foot($)",
+                "Material cost($)",
+                "Labor cost($)",
+                "Tax ($)",
+                "Total ($)");
+        io.print(headings);
+        
+        io.print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (Order currentOrder : orderList) {
+                     
+            String orderInfo = String.format(
+                    "%16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s  %16s",
+                    currentOrder.getCustomerName(),
+                    currentOrder.getStateAbbr(),
+                    currentOrder.getProductType(),
+                    currentOrder.getTaxRate().toString(),
+                    currentOrder.getArea().toString(),
+                    currentOrder.getCostPerSquareFoot().toString(),
+                    currentOrder.getLaborCostPerSquareFoot().toString(),
+                    currentOrder.getMaterialCost().toString(),
+                    currentOrder.getLaborCost().toString(),
+                    currentOrder.getTax().toString(),
+                    currentOrder.getTotal().toString());
+            io.print(orderInfo);
+        }
+        io.print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        return io.readString("Please hit enter to continue");
+        }   
 }
